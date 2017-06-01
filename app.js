d6a882bd7c1d4a737c0b05426b5e35d61580ddba
anatol-blackforest <anatolblackforest@gmail.com>
2017-06-01 20:56:25 +0300

    This file is empty (?)

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+let messages = ["Very big image! (must be less than 2 mb)", "Please upload image only!"];
+
 app.set("twig options", {strict_variables: false});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
@@ -24,16 +26,34 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.post('/', uploader.single('preview'), (req, res, next) => {
-	if(req.query && req.query.ID){
-		deleting(req.query.ID, (err, films) => {
-			res.redirect('/');
-		});
-	}else{
-		add(req, res, (films) => {
-			res.redirect('/');
-		});
-	}
+app.post('/', (req, res, next) => {
+    uploader(req, res, function (err) {
+		if (err){
+			list((err, films) => {
+				res.render('films.twig',{films: films, hint: messages[0]});
+			});
+		} else {
+			if (req.file && req.file.mimetype && req.file.mimetype.indexOf('image') !== -1){
+				add(req, res, (films) => {
+					res.redirect('/');
+				});
+			} else if (req.file && req.file.mimetype && req.file.mimetype.indexOf('image') == -1){
+				list((err, films) => {
+					res.render('films.twig',{films: films, hint: messages[1]});
+				});
+			}else{
+				add(req, res, (films) => {
+					res.redirect('/');
+				});
+			}
+		}
+	});
+});
+
+app.delete('/', (req, res, next) => {
+	deleting(req.query.ID, (err, films) => {
+		res.render('films.twig',{films: films});
+	});
 });
 
 app.listen(3000);
