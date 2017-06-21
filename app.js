51bff7419d8a23a8060ffc0fb16bc5b71d2477ee
anatol-blackforest <anatolblackforest@gmail.com>
2017-06-21 16:47:53 +0300

    Route

--- app.js
+++ app.js
@@ -20,12 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	list((err, films) => {
-		res.render('films.twig',{films: films});
-	});
-});
-
 //Обработчик форм (на добавление и редактирование)
 
 let editor = function(err, req, res, typeOfEdit) {
@@ -51,27 +45,35 @@ let editor = function(err, req, res, typeOfEdit) {
 			}
 		}
 	}
-}
+};
 
-app.post('/', (req, res, next) => {
-    uploader(req, res, function (err) {
-	   
-       if(Boolean(req.body.edit)){
-		    //если редактируем
-			editor(err, req, res, "change");
-	   }else{
-		    //если добавляем новое
-			editor(err, req, res, "add");
-	   }
+app.route("/")
+    //рендерим список фильмов
+	.get((req, res) => {
+		list((err, films) => {
+			res.render('films.twig',{films: films});
+		});
+	})
+    //постим или редактируем
+	.post((req, res, next) => {
+		uploader(req, res, function (err) {
+		
+			if(Boolean(req.body.edit)){
+					//если редактируем
+					editor(err, req, res, "change");
+			}else{
+					//если добавляем новое
+					editor(err, req, res, "add");
+			}
 
+		});
+	})
+    //удаляем
+	.delete((req, res, next) => {
+		deleting(req.query.ID, (err, films) => {
+			res.render('films.twig',{films: films});
+		});
 	});
-});
-
-app.delete('/', (req, res, next) => {
-	deleting(req.query.ID, (err, films) => {
-		res.render('films.twig',{films: films});
-	});
-});
 
 app.listen(3000);
 console.log('Express server listening on port 3000');
